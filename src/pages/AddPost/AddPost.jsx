639c030ee6735a80f19fa74d3460006f9f73b927
gkatsikis <geo.bjj.kat@gmail.com>
2022-03-24 13:27:52 -0400

    add form for addPost

--- src/pages/AddPost/AddPost.jsx
+++ src/pages/AddPost/AddPost.jsx
@@ -2,8 +2,49 @@ import { useState } from 'react';
 
 
 const AddPost = (props) => {
-  return ( 
+  
+
+
+  return (
+    <>
     <h1>Add Post</h1>
+    <div> Title:
+      <label 
+      htmlFor="post-input"
+      className="post-label">
+
+      </label>
+      <input 
+      type="text"
+      className="post-form"
+      name="title"
+       />
+    </div>
+    <div> Story:
+      <label 
+      htmlFor="post-input"
+      className="post-label">
+
+      </label>
+      <textarea 
+      type="text"
+      className="post-form"
+      name="story"
+       />
+    </div>
+    <div>
+      <label 
+      htmlFor="post-input"
+      className="post-label">
+
+      </label>
+      <input 
+      type="file"
+      className="post-form"
+      name="picture"
+       />
+    </div>
+    </> 
    );
 }
  
