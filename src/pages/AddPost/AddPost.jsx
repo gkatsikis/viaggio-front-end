f39bc6533470ccf7e980cf8bc3597fd17c8ad4c0
gkatsikis <geo.bjj.kat@gmail.com>
2022-03-24 13:56:58 -0400

    add addPost functionality

--- src/pages/AddPost/AddPost.jsx
+++ src/pages/AddPost/AddPost.jsx
@@ -2,12 +2,21 @@ import { useState } from 'react';
 
 
 const AddPost = (props) => {
-  
+  const [formData, setFormData] = useState({
+    title: '',
+    story: '',
+  })
 
+  const handleChange = evt => {
+    setFormData({ ...formData, [evt.target.name]: evt.target.value })
+  }
+  
+  const [validForm, setValidForm] = useState(false)
 
   return (
     <>
     <h1>Add Post</h1>
+    <form action="">
     <div> Title:
       <label 
       htmlFor="post-input"
@@ -18,32 +27,45 @@ const AddPost = (props) => {
       type="text"
       className="post-form"
       name="title"
+      value={formData.title}
+      required
+      onChange={handleChange}
        />
     </div>
     <div> Story:
       <label 
       htmlFor="post-input"
       className="post-label">
-
       </label>
       <textarea 
       type="text"
       className="post-form"
       name="story"
+      value={formData.story}
+      required
+      onChange={handleChange}
        />
     </div>
-    <div>
+    {/* <div>
       <label 
       htmlFor="post-input"
       className="post-label">
-
+        Upload Photo
       </label>
       <input 
       type="file"
       className="post-form"
-      name="picture"
+      name="photo"
+      onChange={handleChange}
        />
-    </div>
+    </div> */}
+    <button 
+    type="submit"
+    className="btn"
+    disabled={!validForm}
+    >
+      Add Your Story</button>
+    </form>
     </> 
    );
 }
