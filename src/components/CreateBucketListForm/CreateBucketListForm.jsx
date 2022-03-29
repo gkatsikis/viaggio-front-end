3bf1037e1c2bca7b2ab209ea2f4925836a2324da
zuri227 <zuri22780@gmail.com>
2022-03-29 18:13:50 -0400

    Finish bucket list form

--- src/components/CreateBucketListForm/CreateBucketListForm.jsx
+++ src/components/CreateBucketListForm/CreateBucketListForm.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { create } from '../../services/bucketlistService'
 
-const CreateBucketListForm = () => {
+const CreateBucketListForm = (props) => {
   const [formData, setFormData] = useState({
     location: '',
     season: '',
@@ -9,23 +9,96 @@ const CreateBucketListForm = () => {
     fulfilled: false,
   })
 
+  const handleChange = evt => {
+    console.log('This is:', evt)
+    setFormData({ ...formData, [evt.target.name]: evt.target.value })
+  }
+
+  const [validForm, setValidForm] = useState(false)
+
+  const formElement = useRef()
+
+  const handleSubmit = evt => {
+    evt.preventDefault()
+    const itemFormData = new FormData()
+    itemFormData.append('location', formData.location)
+    itemFormData.append('season', formData.season)
+    itemFormData.append('todo', formData.todo)
+    itemFormData.append('fulfilled', formData.fulfilled)
+    props.handleCreateItem(itemFormData)
+  }
+
+  useEffect(() => {
+    formElement.current.checkValidity() ? setValidForm(true) : setValidForm(false)
+  }, [formData])
 
   return (  
     <>
     <h1>Add to Bucket List</h1>
-    <form autocomplete="off">
+    <form autoComplete="off"
+    ref={formElement}
+    onSubmit={handleSubmit}
+    >
       <div> Location:
         <label htmlFor="item-input"
         className='item-label'>
         </label>
         <input type="text" 
         className='item-form'
+        name="location"
         value={formData.location}
         required
+        onChange={handleChange}
         />
       </div>
 
+      <div> Season: 
+        <label htmlFor="item-input"
+        className="item-label">
+        </label>
+        <input type="text" 
+        className='item-form'
+        name="season"
+        value={formData.season}
+        required
+        onChange={handleChange}
+        />
+      </div>
+
+      <div> To Do: 
+        <label htmlFor="item-input"
+        className="item-label">
+        </label>
+        <input type="text" 
+        className='item-form'
+        name="todo"
+        value={formData.todo}
+        required
+        onChange={handleChange}
+        />
+      </div>
+
+      <div> Done: 
+        <label htmlFor="item-input"
+        className="item-label">
+        </label>
+        <input type="checkbox" 
+        className='item-form'
+        name="fulfilled"
+        value={formData.fulfilled}
+        onChange={handleChange}
+        />
+      </div>
+
+      <button
+      type="submit"
+      className="btn"
+      disabled={!validForm}
+      >
+      Add Bucket List Item
+      </button>
     </form>
+    <hr />
     </>
   );
 }
--- src/pages/BucketList/BucketList.jsx
+++ src/pages/BucketList/BucketList.jsx
@@ -1,16 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import CreateBucketListForm from '../../components/CreateBucketListForm/CreateBucketListForm';
 import ListItem from "../../components/ListItem/ListItem";
+import * as bucketlistService from '../../services/bucketlistService'
 
 const BucketList = () => {
-  const [bucketlists, setBucketList] = useState([])
+  const [listItems, setListItems] = useState([])
 
-
+const handleCreateItem = async(data) => {
+  console.log('NEW LIST ITEM DATA:', data)
+  const newListItem = await bucketlistService.create(data)
+  console.log(newListItem)
+  setListItems([...listItems, newListItem])
+}
 
   return (  
     <>
       <h1>Bucket List</h1>
-      <CreateBucketListForm />
+      <CreateBucketListForm handleCreateItem={handleCreateItem}/>
       <ListItem />
     </>
   );
