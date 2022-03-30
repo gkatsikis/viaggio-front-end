f93f75269a618c49970ba0439aeb1d34ba2de1c3
zuri227 <zuri22780@gmail.com>
2022-03-30 12:43:20 -0400

    Start list item details code

---
+++ src/pages/ListItem/ListItemDetails.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+import * as bucketlistService from '../../services/bucketlistService'
+import ListItem from '../../components/ListItem/ListItem'
+
+const ListItemDetails = (props) => {
+
+  return (  
+    <h1>Here are your list details</h1>
+  );
+}
+
+export default ListItemDetails;
