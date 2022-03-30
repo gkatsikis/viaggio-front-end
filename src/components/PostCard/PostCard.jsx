f924eac0df46d0580c138e42f0a296f9f1b04616
kevanteaniah <kevanteaniah@gmail.com>
2022-03-30 13:58:02 -0400

    Style Card

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -1,26 +1,44 @@
+import styles from './PostCard.css'
 import { Link } from 'react-router-dom'
 
+
 const PostCard = ({post}) => {
+  
   return (  
-    <div className="card">
-      <Link
-      className="post-card"
-      to={`/post/${post._id}`}
-       >
-      <div className="card-body">
-        <h2 className="card-text">{post.title}</h2>
-        <p className="card-text">{post.story}</p>
-        <img 
+  
+    <div className="container">
+    
+    <div className="cardcontainer">
+        <div className="photo"> <img 
         src={post.postPhoto ? post.postPhoto : `https://i.imgur.com/saIoSZF.jpeg`}
         alt="unhappy child canoing"
         className="card-img-top"
         />
+            
+        </div>
+        
+    </div>
+    <div className="content">
+            <p className="txt4">{post.title}</p>
+            <div className='profile-info'><img className="profile-img"src="" alt="" /><p className="txt4">Profile Name</p></div>
+        </div>
+</div>
+    
+    );
+  }
+  
+  export default PostCard;
+  
+  // <div className='card-title'><h2 className="card-text">{post.title}</h2></div>
+// <Link
+//       className="post-card"
+//       to={`/post/${post._id}`}
+//       >
+//         <img 
+//         src={post.postPhoto ? post.postPhoto : `https://i.imgur.com/saIoSZF.jpeg`}
+//         alt="unhappy child canoing"
+//         className="card-img-top"
+//         />
+//       </Link>
 
-      </div>
-      </Link>
-    </div>  
-
-  );
-}
-
-export default PostCard;
+// <h2 className="card-text">{post.title}</h2>
