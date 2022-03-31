12d1091367a96e5b88a55855991e8d854ee44f8e
gkatsikis <geo.bjj.kat@gmail.com>
2022-03-31 15:20:52 -0400

    change getPostById function

--- src/services/postService.js
+++ src/services/postService.js
@@ -17,15 +17,20 @@ function getAll() {
   .then(res => res.json())
 }
 
-const getPostById = async (postId) => {
-  try {
-    const res = await fetch(`${BASE_URL}/${postId}`)
-    const data = await res.json()
-    return data
-  } catch (error) {
-    throw error
-  }
+function getPostById(postId) {
+  return fetch(`${BASE_URL}/${postId}`)
+  .then(res => res.json())
 }
+
+// const getPostById = async (postId) => {
+//   try {
+//     const res = await fetch(`${BASE_URL}/${postId}`)
+//     const data = await res.json()
+//     return data
+//   } catch (error) {
+//     throw error
+//   }
+// }
 
 async function update(post){
   const res = await fetch(`${BASE_URL}/${post._id}`, {
