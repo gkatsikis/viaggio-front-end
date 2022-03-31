6eaf0147050ccfafbba894a01e95cb2a613537ea
gkatsikis <geo.bjj.kat@gmail.com>
2022-03-31 18:02:11 -0400

    revert functions

--- src/services/postService.js
+++ src/services/postService.js
@@ -18,63 +18,77 @@ function getAll() {
 }
 
 
-// const getPostById = async (postId) => {
-  //   try {
-    //     const res = await fetch(`${BASE_URL}/${postId}`)
-    //     const data = await res.json()
-    //     return data
-    //   } catch (error) {
-      //     throw error
-      //   }
-      // }
+const getPostById = async (postId) => {
+  try {
+    const res = await fetch(`${BASE_URL}/${postId}`)
+    const data = await res.json()
+    return data
+  } catch (error) {
+    throw error
+  }
+}
       
-      // async function update(post){
-        //   const res = await fetch(`${BASE_URL}/${post._id}`, {
-          //     method: 'PUT',
-          //     headers: {'content-type': 'application/json'},
-          //     body: JSON.stringify(post)
-          //   })
-          //   const data = await res.json()
-          //   console.log(data)
-          //   return data
-          // }
+// function getPostById(postId) {
+//   return fetch(`${BASE_URL}/${postId}`)
+//   .then(res => res.json())
+//   .catch(err => {
+//     console.log(err)
+//   })
+// }
+      async function update(post){
+          const res = await fetch(`${BASE_URL}/${post._id}`, {
+              method: 'PUT',
+              headers: {'content-type': 'application/json'},
+              body: JSON.stringify(post)
+            })
+            const data = await res.json()
+            console.log(data)
+            return data
+          }
           
-function getPostById(postId) {
-  return fetch(`${BASE_URL}/${postId}`)
-  .then(res => res.json())
-  .catch(err => {
-    console.log(err)
-  })
+
+// function update(post) {
+//   return fetch(`${BASE_URL}/${post._id}`, {
+//     method: 'PUT',
+//     headers: {'content-type': 'application/json'},
+//     body: JSON.stringify(post)
+//   })
+//   .then(res => res.json())
+// }
+
+// function deletePost(postId) {
+//   return fetch(`${BASE_URL}/${postId}`, {
+//     method: 'DELETE',
+//     headers: { 'Authorization': 'Bearer ' + tokenService.getToken() }
+//   })
+//   .catch(err => {
+//     console.log(err)
+//   })
+// }
+
+const deletePost = async (postId) => {
+  try {
+    await fetch(`${BASE_URL}/${postId}`, {
+      method: 'DELETE',
+      headers: { 'Authorization': 'Bearer ' + tokenService.getToken() }
+    })
+  } catch (error) {
+    throw error
+  }
 }
 
-function update(post) {
-  return fetch(`${BASE_URL}/${post._id}`, {
-    method: 'PUT',
-    headers: {'content-type': 'application/json'},
-    body: JSON.stringify(post)
-  })
-  .then(res => res.json())
-}
-
-function deletePost(postId) {
-  return fetch(`${BASE_URL}/${postId}`, {
-    method: 'DELETE',
-    headers: { 'Authorization': 'Bearer ' + tokenService.getToken() }
-  })
-  .catch(err => {
-    console.log(err)
-  })
-}
-
-// const deletePost = async (postId) => {
-//   try {
-//     await fetch(`${BASE_URL}/${postId}`, {
-//       method: 'DELETE',
-//       headers: { 'Authorization': 'Bearer ' + tokenService.getToken() }
-//     })
-//   } catch (error) {
-//     throw error
-//   }
+// function createComment(postId, comment) {
+//   return fetch(`${BASE_URL}/${postId}/comments`, {
+//     method: 'POST',
+//     headers: {
+//       'content-type': 'application/json',
+//       'Authorization': 'Bearer ' + tokenService.getToken()
+//     },
+//     body: JSON.stringify(comment)
+//   })
+//   .catch(err => {
+//     console.log(err)
+//   })
 // }
 
 const createComment = async (postId, comment) => {
@@ -94,9 +108,13 @@ const createComment = async (postId, comment) => {
   }
 }
 
+// function updateComment(postId, commentId) {
+//   return fetch(`${BASE_URL/${postId}}`)
+// }
+
 const updateComment = async (postId, commentId) => {
   try {
-    const res = await fetch(`${BASE_URL}${postId}/comments/${commentId}`, {
+    const res = await fetch(`${BASE_URL}/${postId}/comments/${commentId}`, {
       method: "PUT",
       headers: {
         'content-type': 'application/json',
