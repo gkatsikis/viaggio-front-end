6e6cfeebb2e182680476dd47b66033544b902bae
kevanteaniah <kevanteaniah@gmail.com>
2022-03-31 15:45:10 -0400

    fix PW bug

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -39,16 +39,4 @@ const PostCard = ({post}) => {
   
   export default PostCard;
   
-  // <div className='card-title'><h2 className="card-text">{post.title}</h2></div>
-// <Link
-//       className="post-card"
-//       to={`/post/${post._id}`}
-//       >
-        // <img 
-        // src={post.postPhoto ? post.postPhoto : `https://i.imgur.com/saIoSZF.jpeg`}
-        // alt="unhappy child canoing"
-        // className="card-img-top"
-        // />
-//       </Link>
-
-// <h2 className="card-text">{post.title}</h2>
+  
